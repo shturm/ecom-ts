e1616c021e1033bc05d9e357a1653788a612e244
Alex Georgiev <alex.georgiev@expertallies.com>
2023-04-26 23:20:52 +0300

    add filters

--- src/pages/ProtectionCategory.tsx
+++ src/pages/ProtectionCategory.tsx
@@ -3,9 +3,10 @@ import { useLoaderData } from 'react-router-dom';
 import data from '../data/products.json';
 import { Product } from '../data/product.types';
 import { ProductTileGrid } from '../components';
+import { Filters, filterProducts } from '../components/Filter';
 
 export async function protectionCategoryLoader(routeData: any) {
-    console.log(routeData);
+    // console.log(routeData);
     const products: Product[] = data as Product[];
     const result: Product[] = products.filter((x) => x.ProtectionCategory == routeData.params.pc);
     return result;
@@ -16,7 +17,8 @@ export interface IProtectionCategoryProps {
 
 export function ProtectionCategory (props: IProtectionCategoryProps) {
     const products: Product[] = useLoaderData() as Product[];
-    console.log("products", products);
+    
+
     return (
     <React.Fragment>
       <ProductTileGrid products={products} />
