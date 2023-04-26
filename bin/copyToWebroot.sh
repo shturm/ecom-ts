17e7b8eb1e3e0640db81753576ab283a3f42b039
shturm <shturm@urghh.net>
2023-04-27 01:31:42 +0300

    add deployment script

---
+++ bin/copyToWebroot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf ~/safetyshoes.bg/assets
+rm -rf ~/safetyshoes.bg/index.html
+rm -rf ~/safetyshoes.bg/vite.svg
+rm -rf ~/safetyshoes.bg/.htaccess
+rm -rf ~/safetyshoes.bg/cart.php
+cp -R dist/* ~/safetyshoes.bg/
